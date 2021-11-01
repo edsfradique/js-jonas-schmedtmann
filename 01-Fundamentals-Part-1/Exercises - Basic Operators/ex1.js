6925426e87920a4dc69bd6d4f48d85e06515d332
Eduardo Fradique <edsfradique@gmail.com>
2021-11-01 12:35:33 -0300

    C7 - Basic Operators

---
+++ 01-Fundamentals-Part-1/Exercises - Basic Operators/ex1.js
@@ -0,0 +1,33 @@
+const continent = 'South American';
+const country = 'Brazil';
+const isIsland = false;
+let population = 250000000;
+const language = 'Portuguese';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// continent = 'Europa';
+// language = 'Japanese';
+
+console.log(population / 2);
+population += 1;
+console.log(population);
+const finland = 6000000;
+console.log(population > finland);
+const averagePeopleCountry = 33000000;
+console.log(population < averagePeopleCountry);
+console.log(
+  country +
+    ' está na ' +
+    continent +
+    ', e seus ' +
+    population +
+    ' de pessoas falam ' +
+    language
+);
+console.log(
+  `${country} está na ${continent}, e seus ${population} de pessoas falam ${language}`
+);
