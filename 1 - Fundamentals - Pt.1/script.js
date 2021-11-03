2134057840d6f6c02ed86ec9e6fd0a749ab1eb86
Eduardo Fradique <edsfradique@gmail.com>
2021-11-03 08:29:31 -0300

    C20 - Statements and Expressions

--- 1 - Fundamentals - Pt.1/script.js
+++ 1 - Fundamentals - Pt.1/script.js
@@ -186,7 +186,6 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log('Someone else should drive...');
 }
-*/
 
 const day = 'monday';
 
@@ -227,3 +226,15 @@ if (day === 'monday') {
 } else {
   console.log('Not a valid day');
 }
+*/
+
+3 + 4;
+1991;
+true && false && !false;
+
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`);
