bdc94026e585d574a28638033fc06ac97e76948e
Eduardo Fradique <edsfradique@gmail.com>
2021-11-03 13:30:37 -0300

    C41 - Functions Declarations vs. Expressions

--- 2 - Fundamentals - Pt.2/script.js
+++ 2 - Fundamentals - Pt.2/script.js
@@ -9,7 +9,6 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
-*/
 
 function logger() {
   console.log('My name is Jonas');
@@ -32,3 +31,20 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+*/
+
+// function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// function expression
+const calcAg2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAg2(1991);
+
+console.log(age1, age2);
