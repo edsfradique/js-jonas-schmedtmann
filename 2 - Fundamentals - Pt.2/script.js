f23bd6cb9e6e5f537a5a90cd8a80447683222553
Eduardo Fradique <edsfradique@gmail.com>
2021-11-04 11:53:03 -0300

    C48 - Dot vs. Bracket Notation

--- 2 - Fundamentals - Pt.2/script.js
+++ 2 - Fundamentals - Pt.2/script.js
@@ -186,7 +186,6 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
   console.log('You have a friend called Steven');
 }
-*/
 
 const jonasArray = [
   'Jonas',
@@ -203,3 +202,42 @@ const jonas = {
   job: 'teacher',
   friends: ['Michael', 'Peter', 'Steven']
 };
+*/
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+
+// console.log(jonas);
+
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
+
+// const namekey = 'Name';
+// console.log(jonas['first' + namekey]);
+// console.log(jonas['last' + namekey]);
+
+// const input = prompt(
+//   'what do you want to know about Jonas? Choose between firstName, lastName, age, job and friends'
+// );
+// // console.log(jonas[input]);
+
+// if (jonas[input]) {
+//   console.log(jonas[input]);
+// } else {
+//   console.log(
+//     'Wrong request! Choose between firstName, lastName, age, job and friends'
+//   );
+// }
+
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschmedtman';
+// console.log(jonas);
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);
