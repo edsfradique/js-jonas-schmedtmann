7466d028e46829bf90a07cf9f5f281790b7182cb
Eduardo Fradique <edsfradique@gmail.com>
2021-11-04 14:42:42 -0300

    C50 - Iterations: The for Loop

---
+++ 2 - Fundamentals - Pt.2/Exercises - Iteration: The for Loop/ex1.js
@@ -0,0 +1,7 @@
+// Há eleições em seu país! Em uma pequena cidade, existem apenas 50 eleitores.
+// Use um loop for para simular a votação de 50 pessoas, registrando uma string como esta para
+// o console (para os números 1 a 50): 'O eleitor número 1 está votando no momento'
+
+for (let i = 1; i <= 50; i += 1) {
+  console.log(`O eleitor número ${i} está votando no momento`);
+}
--- 2 - Fundamentals - Pt.2/script.js
+++ 2 - Fundamentals - Pt.2/script.js
@@ -240,7 +240,6 @@ console.log(jonas);
 console.log(
   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
 );
-*/
 
 const jonas = {
   firstName: 'Jonas',
@@ -275,3 +274,19 @@ console.log(jonas.age);
 // console.log(jonas['calcAge']());
 
 console.log(jonas.getSummary());
+*/
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+for (let i = 1; i <= 10; i += 1) {
+  console.log(`Lifting weights repetition ${i}`);
+}
