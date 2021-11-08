f46831c36c78d2db5272531651ff621b7aa65743
Eduardo Fradique <edsfradique@gmail.com>
2021-11-08 12:25:45 -0300

    memo: PROJECT#1: update 4

--- PROJECT'S/Guess My Number/script.js
+++ PROJECT'S/Guess My Number/script.js
@@ -18,17 +18,35 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // Valor não permitido
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+
+    // Player vence
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    // Tentativa acima
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high!';
-    score -= 1;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').textContent = 'You lose the game!';
+    }
+
+    // Tentativa abaixo
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too low!';
-    score -= 1;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').textContent = 'You lose the game!';
+    }
   }
 });
