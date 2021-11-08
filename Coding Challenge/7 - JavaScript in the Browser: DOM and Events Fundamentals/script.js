a9f09dadc375d2247f72cd8d90b482452aaae533
Eduardo Fradique <edsfradique@gmail.com>
2021-11-08 19:19:12 -0300

    memo: PROJECT#1: update 6

--- Coding Challenge/7 - JavaScript in the Browser: DOM and Events Fundamentals/script.js
+++ Coding Challenge/7 - JavaScript in the Browser: DOM and Events Fundamentals/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// Implemente uma funcionalidade de descanso do jogo, para que o jogador possa fazer uma nova suposição!
+
+// Selecione o elemento com a classe 'novamente' e anexe um manipulador de eventos de clique
+
+// Na função de manipulador, restaure os valores iniciais da 'pontuação' e
+// Variáveis ​​'secretNumber'
+
+// Restaura as condições iniciais da mensagem, número, pontuação e entrada de estimativa
+// Campos
+
+// Também restaure a cor de fundo original (# 222) e a largura do número (15rem)
+
 const bCheck = document.querySelector('.check');
 const bReset = document.querySelector('.again');
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
