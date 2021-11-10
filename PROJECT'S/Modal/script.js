f3c177f92414bdbbaff67f3558dba98644b2a241
Eduardo Fradique <edsfradique@gmail.com>
2021-11-10 12:15:17 -0300

    :memo: PROJECT#2: update 2

--- PROJECT'S/Modal/script.js
+++ PROJECT'S/Modal/script.js
@@ -6,7 +6,7 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('button clicked');
+  // console.log('button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -22,3 +22,10 @@ for (let i = 0; i < btnsOpenModal.length; i += 1) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  // console.log(event);
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
