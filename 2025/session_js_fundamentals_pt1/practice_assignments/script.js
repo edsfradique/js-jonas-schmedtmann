c91ed07648961512c41bfea68ef1ab6c311494ec
Eduardo Fradique <edsfradique@gmail.com>
2025-02-03 13:10:11 -0300

    Practice: Values and Variables

---
+++ 2025/session_js_fundamentals_pt1/practice_assignments/script.js
@@ -0,0 +1,9 @@
+let country = "Brasil";
+let continet = "América do Sul";
+let population = 216;
+
+console.log(country);
+console.log(continet);
+console.log(population);
+
+console.log(country, continet, population);
