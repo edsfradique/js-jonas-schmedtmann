b24fe2bd656e6378f7e657e16e31bb48df8c6af4
Eduardo Fradique <edsfradique@gmail.com>
2025-02-03 13:04:23 -0300

    Lesson: Values and Variables

--- 2025/session_js_fundamentals_pt1/script.js
+++ 2025/session_js_fundamentals_pt1/script.js
@@ -1,4 +1,25 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log("23");
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
